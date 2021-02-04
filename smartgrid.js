27369c0799cad5e7883895949311bf9640cfdd1b
Ilya <olovyannikov_barista@icloud.com>
2021-02-04 20:40:56 +0300

    Добавляет файлы

---
+++ smartgrid.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    outputStyle: 'scss',
+    columns: 12,
+    offset: '40px',
+    mobileFirst: true,
+    container: {
+        maxWidth: '1440px',
+        fields: '20px'
+    },
+    breakPoints: {
+        lg: {
+            width: '1200px',
+            fields: '160px'
+        },
+        md: {
+            width: '992px',
+            fields: '40px'
+        },
+        sm: {
+            width: '720px'
+        },
+        xs: {
+            width: '576px'
+        }
+    },
+};
